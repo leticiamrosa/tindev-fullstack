be965c1f1a16352320d6a1ec29367745e8130444
leticiamrosa <leticiamonteiro.design@gmail.com>
2019-08-07 23:43:20 -0300

    backend done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const DevController = require("./controllers/DevController");
+const LikeController = require("./controllers/LikeController");
+const DislikeController = require("./controllers/DislikeController");
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-  // ?=name${queryNameHere}
-  return res.json({ message: `Servidor online ${req.query.name}` });
-});
+// routes.get("/", (req, res) => {
+//   // ?=name${queryNameHere}
+//   return res.json({ message: `Servidor online ${req.query.name}` });
+// });
 
-routes.post("/devs", (req, res) => {
-  return res.json(req.body);
-});
+routes.post("/devs", DevController.store);
+routes.get("/devs", DevController.index);
+
+routes.post("/devs/:devId/likes", LikeController.store);
+routes.post("/devs/:devId/dislikes", DislikeController.store);
 
 module.exports = routes;
